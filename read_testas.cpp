3d7c93b2c96cdcfd9f6dd46a964a49011aef221c
Saimonas <sudintassaimonass@gmail.com>
2025-02-28 22:59:35 +0200

    Clean up draft

--- read_testas.cpp
+++ read_testas.cpp
@@ -29,11 +29,6 @@ tempStudentInfo parseStudentInfo(std::string& myLine) {
         stream >> student.surname;
     } 
     if (arg >= 2) {
-        //for (int i = 0; i < 16; i++) {
-        //stream >> student.grades[student.count];
-        //student.count++;
-        //stream >> hi >> hii;
-        //cout << hi << " " << hii << endl;
         for (int i = 0; i < (actualLength - student.name.length() - student.surname.length()); i++) {
         stream >> hi;
         if (hi == 10) {
@@ -41,8 +36,6 @@ tempStudentInfo parseStudentInfo(std::string& myLine) {
         }
         student.grades.push_back(hi);};
     };
-    //cout << student.name << " " << student.surname;
-    //studentGroup.push_back(student);
     return student;
 }
 
@@ -58,42 +51,9 @@ int main() {
     // Read from the text file
     ifstream MyReadFile("kursiokai.txt");
 
-    // Use a while loop together with the getline() function to read the file line by line
-    //while (getline (MyReadFile, myText)) {
-        // Output the text from the file
-       //cout << myText << endl;
-    //}
-
     int lineCount = 2;
 
-    //cout << "Type number of lines to read: ";
-    //cin >> lineCount;
-
     getline(MyReadFile, junkText);
-
-    //getline(MyReadFile, myText);
-
-    //cout << myText; 
-    
-    //while (MyReadFile >> student.name >> student.surname >> student.grades[0]);
-
-    //student.count = 0;
-
-    /*while (!MyReadFile.eof( )) {
-          MyReadFile >> student.name >> student.surname >> student.grades[student.count];
-          MyReadFile >> student.grades;
-          student.count++;
-          studentGroup.push_back(student);
-     }*/
-
-     /*for (int i = 0; i < lineCount; i++) {
-        MyReadFile >> student.name >> student.surname;
-        while (!MyReadFile.eol()) {
-
-        }
-
-        }
-     }*/
 
      while (getline (MyReadFile, myLine)) {
         //const auto args = parseArgs(myLine);
